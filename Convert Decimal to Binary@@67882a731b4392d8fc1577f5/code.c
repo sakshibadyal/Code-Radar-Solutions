2aaee58037818eb27e89c6e5588885b33f881b4d
sakshibadyal <sakshibadyal09@gmail.com>
2025-02-03 16:14:17 +0530

    Solution to Convert Decimal to Binary

---
+++ Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int decimal;
+    int size=16;
+    scanf("%d",&decimal);
+    if (decimal==0){
+    printf("0");
+    return 0;
+    }
+    int leadingZeroes=1;
+    for(int i=size-1;i>=0;i--){
+        int bit=(decimal>>i)&1;
+        if(bit==1){
+        leadingZeroes=0;
+
+    }
+    if(!leadingZeroes){
+        printf("%d",bit);
+    }
+}
+printf("\n")
+}
