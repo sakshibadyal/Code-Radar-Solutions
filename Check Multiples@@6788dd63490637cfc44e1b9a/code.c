2e8fd0fb7e334c1600ebc49ca99dae5d92f6d7bf
sakshibadyal <sakshibadyal09@gmail.com>
2025-02-03 11:55:55 +0530

    Solution to Check Multiples

--- Check Multiples@@6788dd63490637cfc44e1b9a/code.c
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -3,7 +3,7 @@ int main(){
     int a,b;
     scanf("%d %d",&a,&b);
     if(b==0){
-        printf("Divison with zero is not allowed.");
+        printf("Division with zero is not allowed.");
     }
     elsd{
         if (a%b==0){
