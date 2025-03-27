a8d52e316b6222c6afecd320f4d2cd4443a4f30c
Sakshi <sakshibadyal09@gmail.com>
2025-03-27 18:49:59 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
+
+void trackPlayerRanks(int ranked[], int n, int player[], int m, int result[]) {
+    // Create a unique ranked leaderboard
+    int unique_ranks[n], rank_size = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || ranked[i] != ranked[i - 1]) {
+            unique_ranks[rank_size++] = ranked[i];
+        }
+    }
+
+    // Find ranks for player scores
+    int index = rank_size - 1;
+    for (int i = 0; i < m; i++) {
+        while (index >= 0 && player[i] >= unique_ranks[index]) {
+            index--;
+        }
+        result[i] = index + 2; // Rank is index + 2
+    }
+}
+
 int main() {
     int n, m;
+    
+    // Read leaderboard size
     scanf("%d", &n);
-    int leaderboard[n], unique_ranks[n], rank_size = 0;
+    int ranked[n];
     for (int i = 0; i < n; i++) {
-        scanf("%d", &leaderboard[i]);
-        if (i == 0 || leaderboard[i] != leaderboard[i - 1]) {
-            unique_ranks[rank_size++] = leaderboard[i]; 
-        }
+        scanf("%d", &ranked[i]);
     }
+
+    // Read number of games
     scanf("%d", &m);
-    int scores[m];
+    int player[m], result[m];
+
     for (int i = 0; i < m; i++) {
-        scanf("%d", &scores[i]);
+        scanf("%d", &player[i]);
     }
-    int index = rank_size - 1; 
+
+    // Call function to compute ranks
+    trackPlayerRanks(ranked, n, player, m, result);
+
+    // Output result
     for (int i = 0; i < m; i++) {
-        while (index >= 0 && scores[i] >= unique_ranks[index]) {
-            index--; 
-        }
-        printf("%d\n", index + 2); 
+        printf("%d\n", result[i]);
     }
+
     return 0;
 }
