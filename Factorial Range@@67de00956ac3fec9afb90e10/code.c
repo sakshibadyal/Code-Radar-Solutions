c4e16f8c05a11574af440d464864fe0b302b906b
Sakshi <sakshibadyal09@gmail.com>
2025-03-28 13:56:30 +0530

    Solution to Factorial Range

--- Factorial Range@@67de00956ac3fec9afb90e10/code.c
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -16,6 +16,24 @@ void factorialRange(int a,int b){
         return ;
     }
     for(int i=a;i<=b;i++){
-        printf("%d\n",factorial(i));
+        printf("%d\n", factorial(i));
     }
+}
+
+#include<stdio.h>
+int factorial(int num){
+int fact=1;
+for(int i=2;i<=num;i++){
+    fact*=i;
+}
+return fact;
+}
+void factorialRange(int a, int b){
+   if(a>b||a<0){
+    printf("Invalid range");
+    return;
+   }
+   for(int i=a;i<=b;i++){
+    printf("%d\n", factorial(i));
+   }
 }
